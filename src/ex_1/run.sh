7361bf98daf8467adbe22ff845e22b2977711da8
ckonstanski <ckonstanski@pippiandcarlos.com>
2017-03-04 22:32:54 -0700

    work

--- src/ex_1/run.sh
+++ src/ex_1/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker run -d --net=host -v /root/docker-presentation/src/ex_1/wwwroot/static:/var/www/html/static test/ubuntu
-#docker run -it -v /root/docker-presentation/src/ex_1/wwwroot/static:/var/www/html/static test/ubuntu /bin/bash
-
+docker run -d --net=host --name test_ubuntu -v /root/docker-presentation/src/ex_1/wwwroot/static:/var/www/html/static test/ubuntu
